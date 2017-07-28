f4f1b0c089872e8874f61565f610a2e89d6b6a90
RuZhang <ru.zhang@newbeeair.com>
2017-07-28 17:55:32 +0800

    Initial Commit

---
+++ CoreTextMagezine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreTextMagezine
+//
+//  Created by Zhang茹儿 on 2017/7/28.
+//  Copyright © 2017年 Ru.zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
